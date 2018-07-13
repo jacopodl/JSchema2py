68c31bba70d268d48207bfa86f2c49f401f7280b
Jacopo De Luca <jacopo.delu@gmail.com>
2018-07-13 12:42:54 +0200

    Added setup

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='jschema2py',
+    version='0.1.0',
+    description="",
+    long_description=open("README.md", "r").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/jacopodl/jschema2py",
+    author="Jacopo De Luca",
+    author_email="jacopo.delu@gmail.com",
+    license='MIT',
+    keywords=["jschema2py", "json", "schema", "jsonschema", "library", "python3"],
+    packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3"
+    ]
+)
